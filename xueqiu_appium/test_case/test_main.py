86ace3d712f1bd1ccf41998022b9ce3409f7359a
Lvan826199 <Lvan826199@163.com>
2021-03-20 18:20:03 +0800

    新增test_base

--- xueqiu_appium/test_case/test_main.py
+++ xueqiu_appium/test_case/test_main.py
@@ -3,17 +3,18 @@ import yaml
 from xueqiu_appium.page.app import App
 import pytest
 
-class TestMain():
+from xueqiu_appium.test_case.test_base import TestBase
+
+
+class TestMain(TestBase):
     '''
     yaml：https://www.runoob.com/w3cnote/yaml-intro.html
     '''
     @pytest.mark.parametrize("value1,value2",yaml.safe_load(open("./test_main.yaml")))
     def test_main(self,value1,value2):
-        app =App()
-        app.start().main().goto_search()
+        self.app.start().main().goto_search()
         print(value1)
         print(value2)
 
     def test_windows(self):
-        app=App()
-        app.start().main().go_to_windows()
+        self.app.start().main().go_to_windows()
