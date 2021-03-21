9f28c5b7ec9d4689071aa8b59558489c3d21f457
Lvan826199 <Lvan826199@163.com>
2021-03-21 19:23:01 +0800

    注释conftest.py文件及yield使用（scrcpy录屏）

--- app/testframework/test_case/conftest.py
+++ app/testframework/test_case/conftest.py
@@ -5,6 +5,11 @@ import subprocess
 
 import pytest
 
+#conftest.py文件就相当于是pytest的配置文件需要配合fixture使用
+# 在执行别的test_files文件的时候，会自动使用这个文件
+
+#yield表示在未执行完全部的时候，不会调用他后面的内容
+# 他后面的内容可以理解成异常处理中的finally，在最后才会执行的步骤
 
 @pytest.fixture(scope='class',autouse=True)
 def record():
